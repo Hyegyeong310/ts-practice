46594014e0dcc719b226fdf156b5a6aaccfa65fe
한혜경 <hyegyeong310@gmail.com>
2019-10-12 00:11:39 +0900

    [Add] typescript class 연습. interface는 단순히 객체의 type을 지정한다면, class는 interface의 기능을 함과 동시에, class 멤버에 대한 구현 정보를 포함할 수 있다(변수, 메소드 등)

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,18 @@
-interface Human {
-  name: string;
-  age: number;
-  gender: string;
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
 
-const person = {
-  name: 'Betty',
-  age: 0,
-  gender: 'female'
-};
+const betty = new Human('Betty', 0, 'female');
 
 const sayHi = (person: Human): string => {
   return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
 };
 
-console.log(sayHi(person));
+console.log(sayHi(betty));
